8341f68a3937656bf75f92dc8bec2db0a48dd4a3
JonasBechler <jonasbechler@gmail.com>
2023-01-16 18:57:02 +0100

    dockerize

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import config from "../../config.json"
 
 import './App.css'
 import './SWK_React_Template/src/styles/App.css'
@@ -10,6 +9,12 @@ import Main_Kn_Login from './Main_Kn_Login';
 function App() {
 	const name = "Login";
 	const icon = require("./KonstanzLoginLogo.png");
+	let config = null;
+	try{
+		config = require( "../../../config.json");
+	}catch(error){
+		config = require( "../config.json");
+	}
 	config.port = config.login.port
 	config.port_react = config.login.port_react
 
